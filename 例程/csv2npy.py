46eeb5036601a9b8c399ae229294980d65d5598a
diego3893 <diegozcx@foxmail.com>
2026-05-14 19:52:33 +0800

    添加新数据

--- 例程/csv2npy.py
+++ 例程/csv2npy.py
@@ -14,25 +14,26 @@ def dataconvert(loadfilename, savefilename):
     np.save(savefilename, data_all)
 
 if __name__ == "__main__":
-    input_dir = "./data/label0" 
+    input_dir = ["./data/label1", "./data/label0"] 
     
-    csv_files = [f for f in os.listdir(input_dir) if f.endswith('.csv')]
+    for dir in input_dir:
+        csv_files = [f for f in os.listdir(dir) if f.endswith('.csv')]
     
-    if not csv_files:
-        print(f"在目录 {input_dir} 下未找到任何 CSV 文件！")
-    else:
-        print(f"共找到 {len(csv_files)} 个 CSV 文件，准备开始转换...")
-        
-        for i, fileName in enumerate(csv_files):
-            load_path = os.path.join(input_dir, fileName)
+        if not csv_files:
+            print(f"在目录 {dir} 下未找到任何 CSV 文件！")
+        else:
+            print(f"共找到 {len(csv_files)} 个 CSV 文件，准备开始转换...")
             
-            save_name = f"data{i+1}.npy"
-            save_path = os.path.join(input_dir, save_name)
-            
-            try:
-                dataconvert(load_path, save_path)
-                print(f"成功: {fileName} -> {save_name}")
-            except Exception as e:
-                print(f"跳过文件 {fileName}，发生错误: {e}")
+            for i, fileName in enumerate(csv_files):
+                load_path = os.path.join(dir, fileName)
+                
+                save_name = f"data{i+1}.npy"
+                save_path = os.path.join(dir, save_name)
+                
+                try:
+                    dataconvert(load_path, save_path)
+                    print(f"成功: {fileName} -> {save_name}")
+                except Exception as e:
+                    print(f"跳过文件 {fileName}，发生错误: {e}")
 
-        print("所有转换任务已处理完毕。")
+            print("所有转换任务已处理完毕。")
